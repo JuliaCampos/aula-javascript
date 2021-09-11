5220bee897a76c5132fc05f4a4baa4cbbf460a6f
julia <julia.campos31@gmail.com>
2021-09-11 17:48:42 +0000

    tipos de funções

--- index.js
+++ index.js
@@ -43,4 +43,41 @@ console.log(familia);
 console.log(familia[3]);
 
 //funcoes(functions)
+let corSite = "azul";
+let novaCorSite = "vermelho";
+//verbo + substantivo - sem parametro
+function resetaCor(){
+  corSite = "";
+};
 
+console.log("corSite: " + corSite);
+resetaCor();
+console.log("corSite: " + corSite);
+
+//verbo + substantivo - com parametro
+function resetaCorParametro(cor, tonalidade){
+  novaCorSite = cor + tonalidade;
+};
+
+console.log("novaCorSite: " + novaCorSite);
+resetaCorParametro("rosa", " claro");
+console.log("novaCorSite: " + novaCorSite);
+
+//tipos de funçoes
+//1- realiza uma tarefa e não devolve nada
+function dizerNome(){
+  console.log('jhonatan');
+} 
+
+dizerNome();
+
+//2- retorna resultado
+function multiplicarPorDois(valor){
+  return valor * 2;
+}
+
+console.log(multiplicarPorDois(5));
+
+let resultado = multiplicarPorDois(5);
+
+console.log("resultado: " + resultado);
